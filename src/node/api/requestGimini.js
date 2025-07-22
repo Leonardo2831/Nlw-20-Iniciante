c9f7d819c6f2609384c6fcee7ea98d6016dcadb9
Leonardo2831 <leoferraz1818@gmail.com>
2025-07-22 10:48:11 -0300

    initial request gimini, back-end

---
+++ src/node/api/requestGimini.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+import { GoogleGenerativeAI } from  '@google/generative-ai';
+
+dotenv.config();
+
+export default async function requestIA(req, res) {
+    console.log('ta aqui');
+    
+
+    const genIA = new GoogleGenerativeAI({ apiKey: process.env.API_KEY });
+    const modelPro = genIA.getGenerativeModel({ model: 'gemini-2.5-flash'});
+
+    const {input , contextGame} = req.body;
+
+    try {
+        const prompt = `Olha, tenho esse jogo ${contextGame} e queria saber sobre ${input.value}`;
+
+        const result = await modelPro.generateContent(prompt);
+        const response = result.response; 
+
+        console.log('vai funfa');
+        
+        res.status(200).json({ message: response.text() });
+    } catch {
+        console.error("Erro na requisição para a IA:", error);
+        res.status(500).json({ error: "Erro interno do servidor" });
+    }
+
+}
+
+main();
