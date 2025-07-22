2173edb327d8dd527fe3c3cea8c56f218e3ef7d2
Leonardo2831 <leoferraz1818@gmail.com>
2025-07-22 11:48:47 -0300

    new conditional to request

--- src/node/api/requestGemini.js
+++ src/node/api/requestGemini.js
@@ -5,27 +5,29 @@ import { GoogleGenerativeAI } from  '@google/generative-ai';
 dotenv.config();
 
 export default async function requestIA(req, res) {
-    console.log('ta aqui');
-    
     const genIA = new GoogleGenerativeAI({ apiKey: process.env.API_KEY });
     const modelPro = genIA.getGenerativeModel({ model: 'gemini-2.5-flash'});
 
-    const {input , contextGame} = req.body;
+    if(req.method === 'POST'){
+        const {input , contextGame} = req.body;
 
-    try {
-        const prompt = `Olha, tenho esse jogo ${contextGame} e queria saber sobre ${input}`;
+        try {
+            const prompt = `Olha, tenho esse jogo ${contextGame} e queria saber sobre ${input}`;
 
-        const result = await modelPro.generateContent(prompt);
-        const response = result.response; 
+            const result = await modelPro.generateContent(prompt);
+            const response = result.response; 
 
-        console.log('vai funfa');
-        const convertText = new Showdown.Converter();
-        const convertToHtml = convertText.makeHtml(response.text());
-        
-        res.status(200).json({ message: convertToHtml });
-    } catch {
-        console.error("Erro na requisição para a IA:", error);
-        res.status(500).json({ error: "Erro interno do servidor" });
+            console.log('vai funfa');
+            const convertText = new Showdown.Converter();
+            const convertToHtml = convertText.makeHtml(response.text());
+            
+            res.status(200).json({ message: convertToHtml });
+        } catch {
+            console.error("Erro na requisição para a IA:", error);
+            res.status(500).json({ error: "Erro interno do servidor" });
+        }
+    } else {
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`O método ${req.method} não é permitido`);
     }
-
 }
