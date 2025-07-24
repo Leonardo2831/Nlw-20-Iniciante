003ffbf00b4e31398b1e73d7ec8e3dadab8aa13f
Leonardo2831 <leoferraz1818@gmail.com>
2025-07-24 09:07:56 -0300

    new request games

--- api/requestGames.js
+++ api/requestGames.js
@@ -1,4 +1,15 @@
-import dotenv from 'dotenv';
+export default async function getGamesSteam(req, res){
+    if (req.method !== 'GET') {
+        return res.status(405).json({ message: `Método ${req.method} não permitido` });
+    }
 
-dotenv.config();
+    try {
+        const response = await fetch('https://api.steampowered.com/ISteamApps/GetAppList/v2/');
+        const data = await response.json();
 
+        return res.status(200).json(data.applist.apps);
+    } catch (error) {
+        console.error('Erro ao buscar jogos da Steam:', error);
+        return res.status(500).json({ error: 'Erro ao buscar jogos da Steam' });
+    }
+}
